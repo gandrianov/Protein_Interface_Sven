cd8f404461ab0b3095a35a7898e5acbb95de8b11
svenmiller <sven.alexander.miller@gmail.com>
2021-11-01 17:46:45 -0400

    finished implementation of PDB containter and RMSD calc

--- PDBContainer.py
+++ PDBContainer.py
@@ -1,26 +1,91 @@
 class PDBAtom:
-	def __init__():
-		pass
+    def __init__(self, x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
 
 
 class PDBContainer:
-	def __init__():
-		pass
+    def __init__(self, filename):
+        self.rows = self.read_file(filename)
+        self.protein_rows = self.parse_protein_rows(self.rows) # <chain_id>:<list of coordinates>
+        self.protein_coordinates = {key:self.parse_atom(value) for key, value in self.protein_rows.items()}
+        self.ligand_rows = self.parse_ligand_rows(self.rows)
+        # self.ligand_coordinates = {for }
 
-	def read_file():
-		pass
+        self.ligand_coordinates = {}
 
-	def parse_protein_rows():
-		pass
+        for key, value in self.ligand_rows.items():
+            for key2, value2 in value.items():
+                data = self.parse_atom(value2)
+                if key not in self.ligand_coordinates:
+                    self.ligand_coordinates[key] = {}
+                if key2 not in self.ligand_coordinates[key]:
+                    self.ligand_coordinates[key][key2] = data
 
-	def parse_ligand_rows():
-		pass
+        # print(self.ligand_coordinates)
 
-	def parse_atom():
-		pass
+    def read_file(self, filename):
+        with open(filename, 'r') as f:
+            rows= f.read().split('\n')
+        
+        return rows
 
-	def get_ligand_coords():
-		pass
+    def parse_protein_rows(self, rows):
 
-	def get_protein_coords():
-		pass
+        protein_atom = {}
+        for row in rows:
+            if row.startswith('ATOM'):
+                chain_name = [e for e in row.split(" ") if e != ""]
+                chain_name = chain_name[6] + chain_name[-3]
+                if chain_name not in protein_atom:
+                    protein_atom[chain_name] = [row]
+                else:
+                    protein_atom[chain_name].append(row)
+                
+        return protein_atom
+
+    def parse_ligand_rows(self, rows):
+        hets = {}
+        for row in rows:
+            if row.startswith('HETATM'):
+                chain_name = [e for e in row.split(" ") if e != ""]
+                chain_name = chain_name[6] + chain_name[-3]
+                ligand_name = [e for e in row.split(" ") if e != ""][5]
+                # print(ligand_name)
+
+                if chain_name not in hets:
+                    hets[chain_name] = {}
+                if ligand_name not in hets[chain_name]:
+                    hets[chain_name][ligand_name] = []
+                
+                hets[chain_name][ligand_name].append(row)
+
+        return hets
+
+    def parse_atom(self, rows):
+        coordinates = {}
+        for row in rows: 
+            x,y,z = [e for e in row.split(" ") if e != ""][10:13]
+            x,y,z = float(x), float(y), float(z)
+            atom_name = [e for e in row.split(" ") if e != ""][1:4:2]
+            atom_name = "_".join(atom_name)
+            # print(atom_name,x,y,z)
+            atom=PDBAtom(x,y,z)
+            coordinates[atom_name]=atom
+
+        return coordinates
+
+    def get_ligand_coords(self):
+        return self.ligand_coordinates
+
+    def get_protein_coords(self):
+        return self.protein_coordinates
+
+
+if __name__ == '__main__':
+
+    # atom=PDBAtom(0.1, 0.2, 0.3)
+
+    container =  PDBContainer("/Users/svenmiller/PDBrenum/mmCIF/6pai.cif")
+    # print(container.protein_rows.keys())
--- RMSDCalculator.py
+++ RMSDCalculator.py
@@ -1,5 +1,68 @@
-def get_center_of_mass():
-	pass
+import PDBContainer
 
-def get_RMSD():
-	pass
+def get_center_of_mass(atoms):
+	avg_x, avg_y, avg_z = 0.0 , 0.0 , 0.0
+
+	for atom_name, atom_pdb in atoms.items():
+		avg_x += atom_pdb.x
+		avg_y += atom_pdb.y
+		avg_z += atom_pdb.z
+
+	avg_x/= len(atoms)
+	avg_y/= len(atoms)
+	avg_z/= len(atoms)
+
+	return PDBContainer.PDBAtom(avg_x, avg_y, avg_z)
+
+def get_RMSD(lig_coords, protein_coords):
+	
+	best_lig_atom = None
+	best_prot_atom = None
+	RMSD = 99999
+
+	for lig_name, lig_coord in lig_coords.items():
+		for prot_name, prot_coord in protein_coords.items():
+			dx = (lig_coord.x - prot_coord.x) ** 2
+			dy = (lig_coord.y - prot_coord.y) ** 2
+			dz = (lig_coord.z - prot_coord.z) ** 2
+
+			d = (dx + dy + dz) ** 0.5
+
+			# print(d, prot_name)
+
+			if d < RMSD:
+				best_lig_atom = lig_name
+				best_prot_atom = prot_name
+				RMSD = d
+
+	return RMSD, best_prot_atom, best_lig_atom
+
+
+
+if __name__ == '__main__':
+
+    container =  PDBContainer.PDBContainer("/Users/svenmiller/PDBrenum/mmCIF/6pai.cif")
+
+    ligand_coords = container.get_ligand_coords()
+    protein_coords = container.get_protein_coords()
+
+    for lig_chain, value in ligand_coords.items():
+    	for ligand_name, atom_coords in value.items():
+    		# center_mass = {"Center":get_center_of_mass(atom_coords)}
+    		for protein_chain, prot_coords in protein_coords.items():
+
+    			print("====" * 20)
+
+    			# rmsd, atom = get_RMSD(center_mass, prot_coords)
+    			rmsd, patom, latom = get_RMSD(atom_coords, prot_coords)
+    			print(lig_chain, ligand_name, protein_chain, rmsd, len(atom_coords), patom, latom)
+    			
+
+    			print("====" * 20)
+
+    		# 	print(lig_chain, ligand_name, protein_chain, rmsd, len(atom_coords), atom)
+
+
+
+
+	# ligand_center_mass = get_center_of_mass(ligand_coords)
